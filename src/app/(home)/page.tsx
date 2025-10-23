877d4b656b3d7a137df91543902e79c240eec942
shiva <ghassemzadeh.sh@gmail.com>
2025-10-23 21:42:35 +0200

    add sidebar and layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <Image src="/Youtube_logo.png" alt="LOGO" height={50} width={50} />
-      <p className="text-xl font-semibold tracking-tight">New Tube</p>
-    </div>
-  );
+  return <div>I will load video in the future</div>;
 }
