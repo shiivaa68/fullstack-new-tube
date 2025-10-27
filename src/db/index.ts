6aabeeac76f72491a9ca52d8c309e7125b085106
shiva <ghassemzadeh.sh@gmail.com>
2025-10-27 20:36:11 +0100

    Webhook sync

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,5 @@
-import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 
-const db = drizzle(process.env.DATABASE_URL);
-
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql);
